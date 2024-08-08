e59e34fc7f6f248fec33c1c1c984507040042f50
zhouwei <zhouwei@yintatech.com>
2024-08-08 20:06:15 +0800

    整合knife4j接入在线文档中心

--- pet-service/pet-system/src/main/java/com/pidan/system/controller/TestController.java
+++ pet-service/pet-system/src/main/java/com/pidan/system/controller/TestController.java
@@ -3,6 +3,8 @@ package com.pidan.system.controller;
 import com.pidan.common.exception.BusinessException;
 import com.pidan.common.response.HttpStatusEnum;
 import com.pidan.common.response.ResponseResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,16 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2024/8/7 11:06
  * @description
  */
+@Api(tags = "测试模块")
 @RestController
 @RequestMapping("/test")
 public class TestController {
 
     @PostMapping("/normal")
+    @ApiOperation(value = "测试统一结果返回")
     public ResponseResult normal() {
         return ResponseResult.success("test");
     }
 
     @PostMapping("/exception")
+    @ApiOperation(value = "测试统一异常处理")
     public ResponseResult exception() {
         throw new BusinessException(HttpStatusEnum.ERROR);
     }
